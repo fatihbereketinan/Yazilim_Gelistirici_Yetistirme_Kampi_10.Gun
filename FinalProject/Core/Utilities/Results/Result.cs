6e548d3e9ad99ce8da78dd12c29f1064f8e788d7
fatihbereketinan <fatihbereketinan@gmail.com>
2022-09-19 17:24:51 +0300

    Yazilim_Gelistirici_Yetistirme_Kampi_10.Gun

---
+++ FinalProject/Core/Utilities/Results/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+        public Result(bool success)  //constructor overloading,, 
+        {
+            Success = success;
+        }
+
+        public bool Success { get; } //=> throw new NotImplementedException();
+
+        public string Message { get; }
+    }
+}
